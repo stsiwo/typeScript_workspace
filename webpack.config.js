bafd47954d5a69df3475c57ff20a1086e0eda9cb
Satoshi Iwao <stsiwo@gmail.com>
2018-10-27 03:08:13 -0700

    configured webpack and install typeScript

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'TypeScript Workspace',
+      meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'},
+      template: 'src/index.html',
+      inject: true
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    mainFiles: ['index'],
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx']
+  }
+}
